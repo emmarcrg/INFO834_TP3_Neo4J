1f25642f06106bf479617292d1946d3382fc4a4e
Emma Rechon Reguet <emma.rechon@live.fr>
2025-04-09 15:01:54 +0200

    Premières requêtes

--- api_neo4j.py
+++ api_neo4j.py
@@ -1,6 +1,76 @@
 from neo4j import GraphDatabase
 
-uri = "neo4j://TP3:7474"
-driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+class API : 
+    '''
+    Cette classe définit l'ensemble de l'API, par définition, lorsqu'on la lance, elle lance le driver 
+    '''
+    def __init__ (self, mdp):
+        # On lance le driver et une session
+        uri = "neo4j://localhost:7687"
+        
+        # Connexion au driver 
+        self.driver = GraphDatabase.driver(uri, auth=("neo4j", mdp))
+        self.session = self.driver.session()
+    
+    def createTable (self,type, noms, links, type_link): 
+        '''
+        type : le type de l'objet que nous créons => string
+        nom : nom du ou des objet que nous créons => dictionnaire
+        links : ensemble des liens que nous créons => dictionnaire
+        '''
+        self.type_link = type_link
+        nodes = []
+        relationships = []
 
-driver.close() 
+        # Construire les nœuds
+        for nom in noms:
+            nodes.append(f"({nom}:{type} {{ name: '{noms[nom]}'}})")
+
+        # Construire les relations
+        for link in links:
+            relationships.append(f"({link})-[:{type_link} {{miles: {links[link][1]}}}]->({links[link][0]})")
+
+        # Joindre les nœuds et relations avec des virgules
+        cqlCreate = "CREATE " + ", ".join(nodes + relationships)
+        print(cqlCreate)
+        
+        self.session.run(cqlCreate)
+    
+    def getalldata(self, type):
+        cqlGetAll = f"MATCH (x:{type}) RETURN x"
+        data = self.session.run(cqlGetAll)
+        return data
+
+    def getEdge (self, type, universite):
+        cqlGetEdge = f"MATCH (x:{type})-[r]->(y:{universite}) RETURN x.name, y.name"
+        data = self.session.run(cqlGetEdge)
+        return data
+
+    def close_connection(self): 
+        # Ferme la session et le driver
+        self.session.close()
+        self.driver.close() 
+
+if __name__ == '__main__':
+    # On cache le mot de passe
+    with open('logs.txt', 'r', encoding='utf-8') as fichier:
+        for ligne in fichier:
+            mdp = ligne
+    api = API(mdp)
+    
+    api.createTable("university", 
+                    {"cornell": "Cornell University", "yale": "Yale University", "princeton": "Princeton University", "harvard": "Harvard University"}, 
+                    { "cornell" : ("yale", 259),  "cornell" : ("princeton", 210),  "cornell" : ("harvard", 327),
+                     "yale" : ("cornell", 259),  "yale" : ("princeton", 133),  "yale" : ("harvard", 133),
+                     "harvard" : ("cornell", 327),  "harvard" : ("princeton", 260),  "harvard" : ("yale", 133),
+                     "princeton" : ("cornell", 210),  "princeton" : ("harvard", 260),  "princeton" : ("yale", 133)}, 
+                    "connects_in")
+    
+    print("Obtention de toutes les données")
+    all = api.getalldata("university")
+    print(all)
+    
+    print("Obtention des edges avec Harvard")
+    spec = api.getEdge("university", "harvard")
+    print(spec)
+    api.close_connection()
